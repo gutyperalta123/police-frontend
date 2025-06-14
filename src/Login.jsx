867078a2a3c9209521ecfa062d3c9a51b2b1dfc0
Gustavo Peralta <gutyperalta123@gmail.com>
2025-06-14 18:52:57 -0300

    cambio url de localhost por backend online en Render

--- src/Login.jsx
+++ src/Login.jsx
@@ -10,7 +10,7 @@ const Login = ({ onLogin }) => {
     e.preventDefault()
 
     try {
-      const res = await fetch('http://localhost:5000/api/users/login', {
+      const res = await fetch('https://police-backend-dwup.onrender.com/api/users/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password })
