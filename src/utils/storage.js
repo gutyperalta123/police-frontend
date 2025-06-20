18a9af2eaf54ac62d214c98fc67c9e938632c480
Gustavo Peralta <gutyperalta123@gmail.com>
2025-06-19 23:57:29 -0300

    fix: se agregó clearStorage en storage.js

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,23 +1,25 @@
-// src/utils/storage.js
-
-export const API_URL = 'https://police-backend-dwup.onrender.com'
+const TOKEN_KEY = 'token'
+const USER_KEY = 'user'
 
 export const saveToken = token => {
-  localStorage.setItem('token', token)
+  localStorage.setItem(TOKEN_KEY, token)
 }
 
 export const getToken = () => {
-  return localStorage.getItem('token')
-}
-
-export const clearToken = () => {
-  localStorage.removeItem('token')
+  return localStorage.getItem(TOKEN_KEY)
 }
 
 export const saveUser = username => {
-  localStorage.setItem('username', username)
+  localStorage.setItem(USER_KEY, username)
 }
 
 export const getUser = () => {
-  return localStorage.getItem('username')
+  return localStorage.getItem(USER_KEY)
 }
+
+export const clearStorage = () => {
+  localStorage.removeItem(TOKEN_KEY)
+  localStorage.removeItem(USER_KEY)
+}
+
+export const API_URL = 'https://police-backend-dwup.onrender.com'
